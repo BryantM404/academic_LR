e8c7b9f0d93b9e5eda5f0f60ad9c57e06dea43b4
Bryant M <2372055@maranatha.ac.id>
2025-03-25 13:01:56 +0700

    Progress_04.1_Bryant
    Modal Button Delete Superadmin

--- academic_LR/resources/views/layouts/index.blade.php
+++ academic_LR/resources/views/layouts/index.blade.php
@@ -20,6 +20,9 @@
   <link rel="stylesheet" href="{{ asset('css/vertical-layout-light/style.css') }}">
   <!-- endinject -->
   <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
   @yield('ExtraCSS')
 </head>
 <body>
@@ -69,6 +72,9 @@
   <script src="{{ asset('js/dashboard.js') }}"></script>
   <script src="{{ asset('js/Chart.roundedBarCharts.js') }}"></script>
   <!-- End custom js for this page-->
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
   @yield('ExtraJS')
   </body>
 </html>
--- academic_LR/resources/views/superadmin/index.blade.php
+++ academic_LR/resources/views/superadmin/index.blade.php
@@ -63,13 +63,30 @@
                                 </button>
                               </div>
                               <div class="col=3">
-                                
-
-                                <button type="button" class="btn btn-inverse-danger btn-fw btn-sm d-flex align-items-center btn-rounded">
+                                <button type="button" class="btn btn-inverse-danger btn-fw btn-sm d-flex align-items-center btn-rounded"  data-bs-toggle="modal" data-bs-target="#deleteUser">
                                   <i class="mdi mdi-eraser icon-sm"></i> <span class="px-1">delete</span>
                                 </button>
                               </div>
                             </div>
+          
+                            <div class="modal fade" id="deleteUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                              <div class="modal-dialog">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Delete</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div class="modal-body">
+                                    ...
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
                             
                 
                             {{-- <div class="col-sm-6 col-md-4 col-lg-3 ">
